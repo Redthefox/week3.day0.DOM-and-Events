822bc3cf6a618be15e70fcaf69643fea14cfbfa5
Miles <milesgesmallhorne@gmail.com>
2018-07-30 13:25:15 -0400

    do bonus

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,5 @@
+document.body.style.background = "#d3c26f";
+
+document.getElementsByClassName("name")[0].style.textShadow = "-6px 5px yellow";
+document.getElementsByClassName("name")[1].style.textShadow = "-6px 5px yellow";
+document.getElementsByClassName("name")[0].blur("2px");
---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,4 @@
+document.querySelector("h1").innerHTML = "Frostfang"
+
+
+document.querySelector("img").src = "assets/Frostfang_item.png";
